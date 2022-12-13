ad6a8a79717be6c3a5c4a72ae035ff4fff855a83
eswaribala <parameswaribala@gmail.com>
2022-12-13 15:45:05 +0530

    updated

--- BankingApp/BankingApp/Models/FullName.cs
+++ BankingApp/BankingApp/Models/FullName.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BankingApp.Models
 {
@@ -8,12 +9,15 @@ namespace BankingApp.Models
     {
         [Required,RegularExpression("[A-Za-z]+",ErrorMessage ="Pattern Not Matching")]
         [StringLength(50,MinimumLength =4,ErrorMessage ="First Name Should be between 4 and 50 characters")]
+        [Column("First_Name")]
         public string? FirstName { get; set; }
         [Required, RegularExpression("[A-Za-z]+", ErrorMessage = "Pattern Not Matching")]
         [StringLength(50, MinimumLength = 4, ErrorMessage = "Last Name Should be between 4 and 50 characters")]
+        [Column("Last_Name")]
         public string? LastName { get; set; }
         [RegularExpression("[A-Za-z]+", ErrorMessage = "Pattern Not Matching")]
         [StringLength(50, MinimumLength = 4, ErrorMessage = "Middle Name Should be between 4 and 50 characters")]
+        [Column("Middle_Name")]
         public string? MiddleName { get; set; }
     }
 }
