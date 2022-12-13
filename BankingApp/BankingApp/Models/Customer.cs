24fc2e9d1fae3cd5f38707d68e4d67ff0026900c
eswaribala <parameswaribala@gmail.com>
2022-12-13 15:43:42 +0530

    updated

--- BankingApp/BankingApp/Models/Customer.cs
+++ BankingApp/BankingApp/Models/Customer.cs
@@ -1,20 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BankingApp.Models
 {
+    [Table("Customer")]
     public class Customer:IDisposable
     {
         [Key]
         [Required]
+        [Column("Account_No")]
         public long AccountNo { get; set; }
         public FullName? FullName { get; set; }
-        
+        [Column("Contact_No")]
         public long ContactNo { get; set; }
         [Required,RegularExpression("^[a-zA-Z0-9.!#$%&amp;'^_`{}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$",ErrorMessage ="Pattern Not Valid")]
         [StringLength(150, MinimumLength = 10, ErrorMessage = "Email Should be between 10 and 150 characters")]
+        [Column("Email")]
         public string? Email { get; set; }
         [Required]
         [StringLength(10, MinimumLength = 4, ErrorMessage = "Password Should be between 4 and 10 characters")]
+        [Column("Password")]
         public string? Password { get; set; }
 
         public void Dispose()
